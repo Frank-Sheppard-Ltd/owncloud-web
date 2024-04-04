a6faffb3c2e0df6b131d79213adfc3d1f3bd23e3
Florin Ciobanu <florin.ciobanu@franksheppard.com>
2024-04-04 13:56:19 +0100

    updated

--- dev/docker/oc10.config.php
+++ dev/docker/oc10.config.php
@@ -3,5 +3,6 @@ $CONFIG = [
     'web.baseUrl' => 'http://host.docker.internal:8080/index.php/apps/web',
     'web.rewriteLinks' => true,
     'sharing.showPublicLinkQuickAction' => true,
+	'files_external_allow_create_new_local' => 'true',
 ];
 
